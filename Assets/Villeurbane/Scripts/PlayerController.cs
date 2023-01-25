0809550eaf4de606f8e5f46c3436b5eceb6cdef2
julien-martins <39356114+julien-martins@users.noreply.github.com>
2023-01-25 22:48:15 +0100

    Add flashlight, can pause the walker and embed to watch video

--- Assets/Villeurbane/Scripts/PlayerController.cs
+++ Assets/Villeurbane/Scripts/PlayerController.cs
@@ -10,9 +10,10 @@ public class PlayerController : MonoBehaviour
 
     private int pathIndex = 0;
     private bool stop = false;
-
+    private bool pause = false;
+    
     void Start()
-    {
+    {   
         navMeshAgent.SetDestination(path[pathIndex].position);
         pathIndex++;
     }
@@ -20,6 +21,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            pause = !pause;
+        }
+
+        if (pause) {
+            navMeshAgent.speed = 0.0f;
+        } else
+        {
+            navMeshAgent.speed = 3.5f;
+        }
+
         if (navMeshAgent.remainingDistance < 10.0f && !stop)
         {
             navMeshAgent.SetDestination(path[pathIndex].position);
